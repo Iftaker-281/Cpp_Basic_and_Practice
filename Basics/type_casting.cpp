ddc7f5589939a8162e33b9f58bacba88e0098a6e
Iftaker Ahmed Tahmid <ahmedeftakhertahmid@gmail.com>
2026-02-04 21:23:26 +0600

    Create type_casting.cpp

---
+++ Basics/type_casting.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char ch = 'A';
+    double n = 100.90;
+
+    int value = ch;
+    int result = (int)n;
+
+    cout<<value<<endl;
+    cout<<result<<endl;
+
+    return 0;
+}
