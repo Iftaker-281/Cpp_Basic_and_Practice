d36f1b0317921544802c43fafaa4359f5751e635
Iftaker Ahmed Tahmid <ahmedeftakhertahmid@gmail.com>
2026-02-04 21:22:27 +0600

    Create primitive_data_types.cpp

---
+++ Basics/primitive_data_types.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age = 25;
+    float PI = 3.14;
+    char ch = 'A';
+    double num = 100.99;
+    bool issafe = true;
+
+    cout<<age<<endl;
+    cout<<PI<<endl;
+    cout<<ch<<endl;
+    cout<<num<<endl;
+    cout<<issafe<<endl;
+
+    return 0;
+}
